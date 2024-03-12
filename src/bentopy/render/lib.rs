97c8c15c4ee123da7984e4aa8a5f09723a430a2c
Marieke Westendorp <ma3ke.cyber@gmail.com>
2024-03-12 19:55:10 +0100

    Clean up argument preparation

--- src/bentopy/render/lib.rs
+++ src/bentopy/render/lib.rs
@@ -16,10 +16,9 @@ fn py_render_placements(
 ) -> std::io::Result<()> {
     eprintln!("Hello, world");
     let mode = mode
-        .map(|m| Mode::from_str(&m).ok())
-        .flatten()
+        .and_then(|m| Mode::from_str(&m).ok())
         .unwrap_or_default();
-    let limits = limits.map(|l| Limits::from_str(&l).ok()).flatten();
+    let limits = limits.and_then(|l| Limits::from_str(&l).ok());
     render(input_path, output_path, topol_path, root, limits, mode)
 }
 
