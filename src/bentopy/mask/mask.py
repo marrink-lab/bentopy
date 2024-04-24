9db60bfb8341b3b730f395f85995fbf9f06bfde2
Marieke Westendorp <ma3ke.cyber@gmail.com>
2024-04-24 13:18:16 +0200

    Correctly implement the containment plot behavior

--- src/bentopy/mask/mask.py
+++ src/bentopy/mask/mask.py
@@ -110,10 +110,25 @@ def main(args):
     log("Found the following components:")
     log(f"\troot:\t{containment.get_root_components()}")
     log(f"\tleaf:\t{containment.get_leaf_components()}")
-    # TODO: Add an interactive question about whether to plot and if so what path if it is not provided through the args.
-    if args.plot is not None:
-        log(f"Plotting to {args.plot}.")
-        containment.plot(name=str(args.plot))
+
+    # Write the plot of the containments if desired.
+    if args.plot is None and args.interactive:
+        log("Do you want to write an interactive plot to inspect the containments?")
+        log("Leave empty to skip or provide an output path.")
+        while True:
+            path = input("(html) -> ").strip()
+            if len(path) == 0:
+                plot_path = None
+                break
+            plot_path = Path(path)
+            if plot_path.suffix == ".html":
+                break
+            log(f"Path must have an html extension. Found '{plot_path.suffix}'.")
+    else:
+        plot_path = args.plot
+    if plot_path is not None:
+        log(f"Plotting to {plot_path}.")
+        containment.plot(name=str(plot_path))
 
     label_array = containment.data["relabeled_combined_label_array"]
     if args.inspect_labels_path is not None:
