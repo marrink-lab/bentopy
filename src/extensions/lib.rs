3109db3fa0ab2d785152b912160057c1155129f2
Marieke Westendorp <ma3ke.cyber@gmail.com>
2024-06-11 11:50:39 +0200

    Break out render function to a seperate module
    Nice for the upcoming change where I introduce another extension for
    voxelization.

--- src/extensions/lib.rs
+++ src/extensions/lib.rs
@@ -1,43 +1,11 @@
 #![allow(dead_code)]
-
-use std::{path::PathBuf, str::FromStr};
-
 use pyo3::prelude::*;
-use render_placements::{render, Limits, Mode, ResnumMode};
 
-#[pyfunction]
-fn py_render_placements(
-    input_path: PathBuf,
-    output_path: PathBuf,
-    topol_path: Option<PathBuf>,
-    root: Option<PathBuf>,
-    limits: Option<String>,
-    mode: Option<String>,
-    resnum_mode: Option<String>,
-) -> std::io::Result<()> {
-    eprintln!("Hello, world");
-    let mode = mode
-        .and_then(|m| Mode::from_str(&m).ok())
-        .unwrap_or_default();
-    let resnum_mode = resnum_mode
-        .and_then(|m| ResnumMode::from_str(&m).ok())
-        .unwrap_or_default();
-    let limits = limits.and_then(|l| Limits::from_str(&l).ok());
-    render(
-        input_path,
-        output_path,
-        topol_path,
-        root,
-        limits,
-        mode,
-        resnum_mode,
-    )
-}
+mod render;
 
-/// Read xtc files, fast.
 #[pymodule]
 fn _extensions(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(py_render_placements, m)?)?;
+    m.add_function(wrap_pyfunction!(render::py_render_placements, m)?)?;
 
     Ok(())
 }
