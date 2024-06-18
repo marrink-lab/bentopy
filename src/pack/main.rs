afcd6e835ba0814eed53d564e023b211f755fbac
Marieke Westendorp <ma3ke.cyber@gmail.com>
2024-06-18 19:39:09 +0200

    Create a Summary struct

--- src/pack/main.rs
+++ src/pack/main.rs
@@ -11,27 +11,42 @@ mod config;
 mod placement;
 mod state;
 
-fn summary(entries: &[(String, usize, usize, usize, f64)], packing_duration: f64) {
-    println!("Summary:");
-    println!("  idx \tname      \tnrots\ttarget\tplaced\ttime (s)\tremark");
-    println!("  ----\t----------\t-----\t------\t------\t--------\t------");
-    let mut nrots_tot = 0;
-    let mut target_tot = 0;
-    let mut hits_tot = 0;
-    for (i, (name, target, hits, nrots, duration)) in entries.iter().enumerate() {
-        let ok = if hits == target { " " } else { "<" };
-        println!("{i:>4}\t{name:>10}\t{nrots:>5}\t{target:>6}\t{hits:>6}\t{duration:8.2}\t{ok}");
-        nrots_tot += nrots;
-        target_tot += target;
-        hits_tot += hits;
+struct Summary {
+    entries: Vec<(String, usize, usize, usize, f64)>,
+}
+
+impl Summary {
+    fn new() -> Self {
+        Self {
+            entries: Vec::new(),
+        }
     }
-    println!(
-        "    \t          \t{nrots_tot:>5}\t{target_tot:>6}\t{hits_tot:>6}\t{packing_duration:8.2}"
-    )
+
+    fn push(&mut self, name: String, nrots: usize, target: usize, placed: usize, time: f64) {
+        self.entries.push((name, nrots, target, placed, time))
+    }
+
+    fn present(&self, packing_duration: f64) {
+        println!("  idx \tname      \tnrots\ttarget\tplaced\ttime (s)\tremark");
+        println!("  ----\t----------\t-----\t------\t------\t--------\t------");
+        let mut nrots_tot = 0;
+        let mut target_tot = 0;
+        let mut hits_tot = 0;
+        for (i, (name, target, hits, nrots, duration)) in self.entries.iter().enumerate() {
+            let ok = if hits == target { " " } else { "<" };
+            println!(
+                "{i:>4}\t{name:>10}\t{nrots:>5}\t{target:>6}\t{hits:>6}\t{duration:8.2}\t{ok}"
+            );
+            nrots_tot += nrots;
+            target_tot += target;
+            hits_tot += hits;
+        }
+        println!( "    \t          \t{nrots_tot:>5}\t{target_tot:>6}\t{hits_tot:>6}\t{packing_duration:8.2}")
+    }
 }
 
 fn main() -> io::Result<()> {
-    println!("Welcome to new pack. Let's see if I can finish this by Friday.");
+    eprintln!("Welcome to new pack. Let's see if I can finish this by Friday.");
 
     // Preparation.
     let args = Args::parse();
@@ -40,16 +55,15 @@ fn main() -> io::Result<()> {
 
     // Packing.
     let mut placements = Vec::new();
-    let mut entries = Vec::new(); // Log some information for the summary.
+    let mut summary = Summary::new();
     let packing_start = std::time::Instant::now();
-
-
 
     let packing_duration = std::time::Instant::now()
         .duration_since(packing_start)
         .as_secs_f64();
     eprintln!("Packing process took {packing_duration:.3} s.");
 
+    // Output.
     let placement_list_path = format!(
         "{}_placements.json",
         state.output.dir.join(&state.output.title).to_str().unwrap()
@@ -59,7 +73,9 @@ fn main() -> io::Result<()> {
     serde_json::to_writer(placement_list_file, &placement_list)?;
     eprintln!("Wrote placement list to {placement_list_path:?}.");
 
-    summary(&entries, packing_duration);
+    // Final summary.
+    eprintln!("Summary:");
+    summary.present(packing_duration);
 
     Ok(())
 }
