2f7391811b62924280bf15b1ac3422268ed4e635
Marieke Westendorp <ma3ke.cyber@gmail.com>
2024-06-19 17:34:24 +0200

    Implement the actual packing routine

--- src/pack/main.rs
+++ src/pack/main.rs
@@ -1,9 +1,11 @@
 use std::io;
 
-use args::Args;
 use clap::Parser;
+use glam::Mat3;
+use rand::{seq::SliceRandom, Rng};
+
+use args::Args;
 use config::Configuration;
-use glam::U64Vec3;
 use placement::{Batch, Placement, PlacementList};
 use state::State;
 
@@ -33,7 +35,7 @@ impl Summary {
         let mut nrots_tot = 0;
         let mut target_tot = 0;
         let mut hits_tot = 0;
-        for (i, (name, target, hits, nrots, duration)) in self.entries.iter().enumerate() {
+        for (i, (name, nrots, target, hits, duration)) in self.entries.iter().enumerate() {
             let ok = if hits == target { " " } else { "<" };
             println!(
                 "{i:>4}\t{name:>10}\t{nrots:>5}\t{target:>6}\t{hits:>6}\t{duration:8.2}\t{ok}"
@@ -62,57 +64,95 @@ fn main() -> io::Result<()> {
     for segment in &mut state.segments {
         // Prepare the session.
         let start = std::time::Instant::now();
-        let mut placement = Placement::new(segment.name, segment.path);
+        let mut placement = Placement::new(segment.name.clone(), segment.path.clone());
         state.space.enter_session(&segment.compartments);
 
         // Get the free locations.
-        let locations = state.space.get_free_locations();
+        // TODO: Putting it all into a Vec seems dumb and a waste. Maybe we can select a random
+        // subset if the number of locations is very large?
+        let shuffle_guess = segment.number;
+        let mut locations = state.space.get_free_locations().collect::<Vec<_>>();
+        let (mut shuffled, mut locations) =
+            locations.partial_shuffle(&mut state.rng, shuffle_guess);
+        let mut cursor = 0;
 
         let mut hits = 0;
         let mut batch_positions = Vec::new();
-        while hits < segment.number {
+        'placement: while hits < segment.number {
             // TODO: This can become more efficient for successive placement failures.
-            let voxels = segment.get_voxels(state.space.resolution, state.bead_radius);
-            let u = voxels.indices_where::<true>().map(U64Vec3::from_array);
+            let voxels = match segment.voxels() {
+                Some(voxels) => voxels,
+                None => {
+                    segment.voxelize(state.space.resolution, state.bead_radius);
+                    segment.voxels().unwrap()
+                }
+            };
+
+            let [maxx, maxy, maxz] = {
+                let [bx, by, bz] = state.space.dimensions;
+                let [sx, sy, sz] = voxels.dimensions();
+                [bx - sx, by - sy, bz - sz]
+            };
 
             // Pick a random location.
-            let location = todo!();
+            let &location = loop {
+                let candidate = match shuffled.get(cursor) {
+                    // Get it from the shuffled set.
+                    Some(location) => location,
+                    // Or, shuffle more positions if available.
+                    None if !locations.is_empty() => {
+                        (shuffled, locations) =
+                            locations.partial_shuffle(&mut state.rng, shuffle_guess);
+                        cursor = 0;
+                        shuffled.first().unwrap()
+                    }
+                    None => {
+                        // We've gone through all the locations.
+                        // TODO: This is rather unlikely, but must be dealt with correctly. I think
+                        // this may require a nice custom type that does some internal mutabilty
+                        // trickery.
+                        // For now, we'll just break here.
+                        break 'placement;
+                    }
+                };
+                cursor += 1;
+                let &[x, y, z] = candidate;
+                if x < maxx && y < maxy && z < maxz {
+                    break candidate;
+                }
+            };
 
             // Try it.
-            let l = U64Vec3::from_array(location);
-            let v = u.map(|p| p + l);
-            for p in v.map(|p| p.to_array()) {}
             if state.space.check_collisions(voxels, location) {
                 // We found a good spot. Stomp on those stamps!
                 state.space.stamp(voxels, location);
-                batch_positions.push(location);
+                // Transform the location to nm.
+                batch_positions.push(location.map(|v| v as f32 * state.space.resolution));
 
                 // Let's write out the batch and rotate the segment again.
                 // TODO: Perhaps we'll need a little transpose here.
-                let batch = Batch::new(
-                    segment.rotation().to_cols_array_2d(),
-                    batch_positions.clone(),
-                );
+                let batch = Batch::new(segment.rotation, batch_positions.clone());
                 placement.push(batch);
                 batch_positions.clear();
 
-                let rotation = todo!("get a random location"); // TODO: This will be a bloody mess again, won't it?
+                let rotation = Mat3::from_quat(state.rng.gen());
                 segment.set_rotation(rotation);
 
                 hits += 1;
             }
         }
+        state.space.exit_session();
         let duration = std::time::Instant::now().duration_since(start);
 
         // Save the batches.
-        placements.push(placement);
         summary.push(
-            segment.name,
+            segment.name.clone(),
             placement.n_batches(),
             segment.number,
             hits,
             duration.as_secs_f64(),
-        )
+        );
+        placements.push(placement);
     }
 
     let packing_duration = std::time::Instant::now()
