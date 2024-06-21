52b5c92e4a4d39683f9a6ba1616ab5f66670df12
Marieke Westendorp <ma3ke.cyber@gmail.com>
2024-06-21 17:29:02 +0200

    Store Mask as a bitmap

--- src/pack/mask.rs
+++ src/pack/mask.rs
@@ -1,109 +1,204 @@
 pub type Dimensions = [u64; 3]; // TODO: Make into usize? Rather awkward in places right now.
 pub type Position = Dimensions;
 
-// TODO: Bitmap optimization.
-#[derive(Clone)]
+type Backing = u8;
+const BACKING_BITS: usize = Backing::BITS as usize;
+
+#[derive(Debug, Clone)]
 pub struct Mask {
-    dimensions: Dimensions,
-    cells: Box<[bool]>,
+    dimensions: [usize; 3],
+    backings: Box<[Backing]>,
 }
 
 impl Mask {
     pub fn new(dimensions: Dimensions) -> Self {
-        let [w, h, d] = dimensions.map(|v| v as usize);
-        let cells = vec![false; w * h * d].into_boxed_slice();
-        Self { dimensions, cells }
+        let dimensions = dimensions.map(|v| v as usize);
+        let n: usize = dimensions.iter().product();
+        let n_backings = n.div_ceil(BACKING_BITS);
+        Self {
+            dimensions,
+            backings: vec![0; n_backings].into_boxed_slice(),
+        }
     }
 
-    pub fn from_cells(dimensions: [u64; 3], cells: Box<[bool]>) -> Mask {
-        assert_eq!(dimensions.iter().product::<u64>(), cells.len() as u64);
-        Self { dimensions, cells }
+    /// # Panics
+    ///
+    /// If the provided `dimensions` are not compatible with the number of `cells`, this function
+    /// will panic.
+    pub fn from_cells(dimensions: Dimensions, cells: &[bool]) -> Self {
+        let mut mask = Self::new(dimensions);
+        assert_eq!(
+            mask.n_cells(),
+            cells.len(),
+            "the number of cells must match the dimensions"
+        );
+
+        for lin_idx in cells
+            .iter()
+            .enumerate()
+            .filter_map(|(i, &v)| if v { Some(i) } else { None })
+        {
+            mask.set_linear_unchecked::<true>(lin_idx)
+        }
+
+        mask
     }
 
     pub const fn dimensions(&self) -> Dimensions {
-        self.dimensions
+        let [w, h, d] = self.dimensions;
+        [w as u64, h as u64, d as u64]
     }
 
-    pub const fn dimensions_usize(&self) -> [usize; 3] {
-        let [x, y, z] = self.dimensions;
-        [x as usize, y as usize, z as usize]
-    }
-
+    /// Return the number of cells that are equal to `VALUE`.
     pub fn count<const VALUE: bool>(&self) -> usize {
-        self.cells.iter().map(|&c| c as u8 as usize).sum()
-    }
-
-    // TODO: Impl get and get_mut as Deref<Slice>? Would that be nicer?
-    #[inline(always)]
-    pub fn get(&self, idx: Position) -> Option<&bool> {
-        let [w, h, _d] = self.dimensions_usize();
-        let [x, y, z] = idx.map(|v| v as usize);
-        self.cells.get(x + y * w + z * w * h)
-    }
-
-    #[inline(always)]
-    pub fn get_mut(&mut self, idx: Position) -> Option<&mut bool> {
-        let [w, h, _d] = self.dimensions_usize();
-        let [x, y, z] = idx.map(|v| v as usize);
-        self.cells.get_mut(x + y * w + z * w * h)
-    }
-
-    /// Return a three-dimensional index into this [`Mask`] from a linear index.
-    ///
-    /// This function makes no guarantees about whether the returned index is actually within the
-    /// `Mask`.
-    #[inline(always)]
-    fn idx(&self, mut i: usize) -> Position {
-        let [w, h, _d] = self.dimensions_usize();
-
-        let x = i % w;
-        i /= w;
-        let y = i % h;
-        let z = i / h;
-
-        [x as u64, y as u64, z as u64]
-    }
-
-    /// Return an [`Iterator`] over all indices where the the cell is equal to `VALUE`.
-    pub fn indices_where<const VALUE: bool>(&self) -> impl Iterator<Item = Position> + '_ {
-        self.linear_indices_where::<VALUE>().map(|i| self.idx(i))
-    }
-
-    /// Return an [`Iterator`] over all linear indices into the internal `cells` where the the cell
-    /// is equal to `VALUE`.
-    pub fn linear_indices_where<const VALUE: bool>(&self) -> impl Iterator<Item = usize> + '_ {
-        self.cells
+        // If we're counting the `false` cells, we may overcount when there are trailing bits in
+        // the last backing.
+        let overshoot = if VALUE {
+            0
+        } else {
+            self.n_backings() * BACKING_BITS - self.n_cells()
+        };
+        let uncorrected: usize = self
+            .backings
             .iter()
-            .enumerate()
-            .filter_map(|(i, &v)| if v == VALUE { Some(i) } else { None })
+            .map(|backing| {
+                if VALUE {
+                    backing.count_ones() as usize
+                } else {
+                    backing.count_zeros() as usize
+                }
+            })
+            .sum();
+        uncorrected - overshoot
+    }
+
+    const fn n_cells(&self) -> usize {
+        let [w, h, d] = self.dimensions;
+        w * h * d
+    }
+
+    const fn n_backings(&self) -> usize {
+        self.backings.len()
+    }
+
+    const fn backing_idx(&self, lin_idx: usize) -> (usize, usize) {
+        (lin_idx / BACKING_BITS, lin_idx % BACKING_BITS)
+    }
+
+    const fn contains(&self, idx: Position) -> bool {
+        let [x, y, z] = idx;
+        let [w, h, d] = self.dimensions();
+        x < w && y < h && z < d
+    }
+
+    const fn linear_idx(&self, idx: Position) -> usize {
+        let [x, y, z] = idx;
+        let [w, h, _] = self.dimensions;
+        let lin_idx = x as usize + y as usize * w + z as usize * w * h;
+        debug_assert!(lin_idx < self.n_cells());
+        lin_idx
+    }
+
+    const fn get_linear_unchecked(&self, lin_idx: usize) -> bool {
+        debug_assert!(lin_idx < self.n_cells());
+        let (backing_idx, bit_idx) = self.backing_idx(lin_idx);
+        self.backings[backing_idx] >> bit_idx & 1 != 0
+    }
+
+    /// Check whether the cell at the specified `lin_idx` is `VALUE`.
+    const fn query_linear_unchecked<const VALUE: bool>(&self, lin_idx: usize) -> bool {
+        debug_assert!(lin_idx < self.n_cells());
+        let (backing_idx, bit_idx) = self.backing_idx(lin_idx);
+        let backing = self.backings[backing_idx];
+        // FIXME: This seems convoluted for what the truth table actually looks like.
+        if VALUE {
+            match backing {
+                0 => false,
+                u8::MAX => true,
+                _ => backing >> bit_idx & 1 != 0,
+            }
+        } else {
+            match backing {
+                0 => true,
+                u8::MAX => false,
+                _ => backing >> bit_idx & 1 == 0,
+            }
+        }
+    }
+
+    fn set_linear_unchecked<const VALUE: bool>(&mut self, lin_idx: usize) {
+        debug_assert!(lin_idx < self.n_cells());
+        let (backing_idx, bit_idx) = self.backing_idx(lin_idx);
+        if VALUE {
+            let bit = 1 << bit_idx;
+            self.backings[backing_idx] |= bit;
+        } else {
+            let backing = &mut self.backings[backing_idx];
+            *backing &= !(1 << bit_idx);
+        }
+    }
+
+    pub const fn get(&self, idx: Position) -> Option<bool> {
+        if self.contains(idx) {
+            let lin_idx = self.linear_idx(idx);
+            Some(self.get_linear_unchecked(lin_idx))
+        } else {
+            None
+        }
+    }
+
+    /// # Panics
+    ///
+    /// If `idx` is not within the dimensions of this `Mask`, this function will panic.
+    pub fn set(&mut self, idx: Position, value: bool) {
+        assert!(
+            self.contains(idx),
+            "the provided `idx` must be within the dimensions of this mask"
+        );
+        let lin_idx = self.linear_idx(idx);
+        if value {
+            self.set_linear_unchecked::<true>(lin_idx)
+        } else {
+            self.set_linear_unchecked::<false>(lin_idx)
+        }
+    }
+
+    /// Return an [`Iterator`] over all indices where the cell is equal to `VALUE`.
+    pub fn indices_where<const VALUE: bool>(&self) -> impl Iterator<Item = Position> + '_ {
+        let [w, h, d] = self.dimensions();
+        (0..d).flat_map(move |z| {
+            (0..h).flat_map(move |y| {
+                (0..w).filter_map(move |x| {
+                    let idx = [x, y, z];
+                    let lin_idx = self.linear_idx(idx);
+                    if self.query_linear_unchecked::<VALUE>(lin_idx) {
+                        Some(idx)
+                    } else {
+                        None
+                    }
+                })
+            })
+        })
     }
 
     /// Apply some `mask` onto this [`Mask`].
     ///
     /// This is essentially an `or-assign` operation between the `self` and the provided `mask`.
+    /// # Panics
+    ///
+    /// If the dimensions of the stamped and stamping mask are different, this function will panic.
     pub fn apply_mask(&mut self, mask: &Mask) {
-        assert_eq!(self.dimensions, mask.dimensions);
-        assert_eq!(self.cells.len(), mask.cells.len()); // For good measure, so the compiler gets it.
-        self.cells
-            .iter_mut()
-            .zip(mask.cells.iter())
-            .for_each(|(s, &m)| *s |= m);
-    }
+        assert_eq!(
+            self.dimensions(),
+            mask.dimensions(),
+            "the dimensions of both masks must be identical"
+        );
+        // For good measure, so the compiler gets it.
+        assert_eq!(self.n_backings(), mask.n_backings()); // FIXME: Is this one necessary?
 
-    pub fn indices_where_into_vec<const VALUE: bool>(&self, locations: &mut Vec<Position>) {
-        let [w, h, d] = self.dimensions();
-        assert_eq!((w * h * d) as usize, self.cells.len());
-        for z in 0..d {
-            for y in 0..h {
-                for x in 0..w {
-                    let lin_idx = x + y * w + z * w * h;
-                    // Safety: We know that our access is in bounds since `w*h*d` is equal to
-                    // `self.cells.len()`.
-                    if unsafe { *self.cells.get_unchecked(lin_idx as usize) } == VALUE {
-                        locations.push([x, y, z]);
-                    }
-                }
-            }
-        }
+        self.backings
+            .iter_mut()
+            .zip(mask.backings.iter())
+            .for_each(|(s, &m)| *s |= m);
     }
 }
