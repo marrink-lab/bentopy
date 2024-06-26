e30548466fa1785d5a8c9a9afe4c89b7eca1d0a5
Marieke Westendorp <ma3ke.cyber@gmail.com>
2024-06-26 15:09:29 +0200

    Take a min and max value for checking rules
    Additionally, the scalar value in `Position` rules is now described by a
    `Scalar` type alias and it is now a `f32` instead of a `f64`.
    
    Also, the `Rule::At` variant was removed. It may turn up later, but for
    now it is convoluted to implement since it requires a centering system.

--- src/pack/rules.rs
+++ src/pack/rules.rs
@@ -7,18 +7,19 @@ pub enum Axis {
     Z,
 }
 
+type Scalar = f32;
+
 pub enum PositionConstraint {
-    GreaterThan(Axis, f64),
-    LessThan(Axis, f64),
-    At(Axis, f64),
+    GreaterThan(Axis, Scalar),
+    LessThan(Axis, Scalar),
 }
 
 impl PositionConstraint {
     pub fn axis(&self) -> Axis {
         match self {
-            PositionConstraint::GreaterThan(axis, _)
-            | PositionConstraint::LessThan(axis, _)
-            | PositionConstraint::At(axis, _) => *axis,
+            PositionConstraint::GreaterThan(axis, _) | PositionConstraint::LessThan(axis, _) => {
+                *axis
+            }
         }
     }
 }
@@ -31,22 +32,20 @@ impl Rule {
     /// Return whether this rule is satisfied or not.
     ///
     /// The provided `pos` must be in nanometers.
-    pub fn is_satisfied(&self, pos: Vec3) -> bool {
+    pub fn is_satisfied(&self, min: Vec3, max: Vec3) -> bool {
         match self {
             Rule::Position(poscon) => {
                 // TODO: Gotta find a way to know the bounding box or all positions
                 // of the structure placement. We calculate these somewhere already
                 // maybe??
-                let p = match poscon.axis() {
-                    Axis::X => pos.x,
-                    Axis::Y => pos.y,
-                    Axis::Z => pos.z,
-                } as f64;
+                let (min, max) = match poscon.axis() {
+                    Axis::X => (min.x, max.x),
+                    Axis::Y => (min.y, max.y),
+                    Axis::Z => (min.z, max.z),
+                };
                 match poscon {
-                    &PositionConstraint::GreaterThan(_, value) => value > p,
-                    &PositionConstraint::LessThan(_, value) => value < p,
-                    // FIXME: Have a (resolution-sized?) grace edge for the At.
-                    &PositionConstraint::At(_, value) => value == p,
+                    &PositionConstraint::GreaterThan(_, value) => value > max,
+                    &PositionConstraint::LessThan(_, value) => value < min,
                 }
             }
         }
