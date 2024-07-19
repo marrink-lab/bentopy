f7c91ad33efecd5e6d73165bb2d8e0d454fa6be9
Marieke Westendorp <ma3ke.cyber@gmail.com>
2024-07-19 12:49:50 +0200

    Introduce boolean operations for PlaceMap

--- src/placement.rs
+++ src/placement.rs
@@ -18,6 +18,13 @@ pub struct PlaceMap<'s> {
     // Invariant: Any wasted bits are always set to zero.
     // Value is 0 if the place of this solvent bead is not otherwise occupied.
     // Conversely, if the value of a bit is 1, a solvent bead cannot be placed there.
+    //
+    // To illustrate what these wasted bits are, the figure below shows meaningful bits as `*`, and
+    // the wasted bits as `0`. The bytes are stored in first to last order, and the bits in the
+    // bytes are just treated according to their significance.
+    //
+    // ******** ******** 0000****
+    //                   ^^^^-- Wasted bits.
     placements: Box<[u8]>,
 }
 
@@ -88,6 +95,83 @@ impl<'s> PlaceMap<'s> {
         let total = self.solvent.natoms() * self.n_cells();
         total - self.occupied_count()
     }
+
+    /// Repair all wasted bits in the type.
+    fn repair(&mut self) {
+        let n_beads = self.solvent.natoms();
+        let n_bytes = n_beads.div_ceil(8);
+        self.placements
+            .chunks_exact_mut(n_bytes)
+            .map(|chunk| PlacementMut::new(chunk, n_beads))
+            .for_each(|mut p| p.repair())
+    }
+}
+
+impl std::ops::BitOrAssign for PlaceMap<'_> {
+    // Invariant: The or operation will not break the invariant as long as the two operands are
+    // also valid.
+    fn bitor_assign(&mut self, rhs: Self) {
+        assert_eq!(self.dimensions, rhs.dimensions);
+        assert_eq!(self.n_cells(), rhs.n_cells());
+        assert_eq!(self.solvent, rhs.solvent); // FIXME: Possible remove?
+
+        self.placements
+            .iter_mut()
+            .zip(rhs.placements)
+            .for_each(|(s, r)| {
+                *s |= r;
+            });
+    }
+}
+
+impl std::ops::BitAndAssign for PlaceMap<'_> {
+    // Invariant: The or operation will not break the invariant as long as the two operands are
+    // also valid.
+    fn bitand_assign(&mut self, rhs: Self) {
+        assert_eq!(self.dimensions, rhs.dimensions);
+        assert_eq!(self.n_cells(), rhs.n_cells());
+        assert_eq!(self.solvent, rhs.solvent); // FIXME: Possible remove?
+
+        self.placements
+            .iter_mut()
+            .zip(rhs.placements)
+            .for_each(|(s, r)| {
+                *s &= r;
+            });
+    }
+}
+
+impl std::ops::BitXorAssign for PlaceMap<'_> {
+    // Invariant: The or operation will not break the invariant as long as the two operands are
+    // also valid.
+    fn bitxor_assign(&mut self, rhs: Self) {
+        assert_eq!(self.dimensions, rhs.dimensions);
+        assert_eq!(self.n_cells(), rhs.n_cells());
+        assert_eq!(self.solvent, rhs.solvent); // FIXME: Possible remove?
+
+        self.placements
+            .iter_mut()
+            .zip(rhs.placements)
+            .for_each(|(s, r)| {
+                *s ^= r;
+            });
+    }
+}
+
+impl std::ops::Not for PlaceMap<'_> {
+    type Output = Self;
+
+    fn not(mut self) -> Self::Output {
+        // In order to preserve the invariant that the wasted bits are always zero, we need to be
+        // careful, here.
+        // First, we will go over all the placement bytes and apply a not.
+        self.placements.iter_mut().for_each(|s| *s = !(*s));
+
+        // After that, we go and repair the wasted bits, setting them to zero.
+        self.repair();
+
+        self
+    }
 }
 
 pub struct Placement<'ps> {
@@ -141,6 +225,25 @@ impl<'ps> PlacementMut<'ps> {
         let byte = &mut self.bytes[idx / 8];
         *byte |= 1 << (idx % 8);
     }
+
+    /// Repair the wasted bits at the end of this [`PlacementMut`] by setting them to zero.
+    ///
+    /// This operation is idempotent.
+    fn repair(&mut self) {
+        let n_bytes = self.bytes.len();
+        let n_bits = n_bytes * u8::BITS as usize;
+        if n_bits == self.len {
+            return;
+        }
+
+        let Some(last) = self.bytes.last_mut() else {
+            return;
+        };
+        let n_wasted = n_bits - self.len;
+
+        let mask = (1u8 << (u8::BITS as usize - n_wasted)) - 1;
+        *last &= mask;
+    }
 }
 
 pub struct Cookies {
@@ -183,5 +286,64 @@ impl Cookies {
     /// Calculate the offset of a cookie at some position from the system origin.
     pub fn offset(&self, cell_pos: UVec3) -> Vec3 {
         cell_pos.as_vec3() * self.cookie_size
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use eightyseven::{reader::ReadGro, structure::Structure};
+
+    use super::*;
+
+    #[test]
+    fn repair_placement_mut() {
+        let mut bytes = [0xff, 0xff, 0xff];
+        let mut p = PlacementMut::new(&mut bytes, 20);
+
+        assert_eq!(*p.bytes.last().unwrap(), 0b11111111);
+        p.repair();
+        assert_eq!(*p.bytes.last().unwrap(), 0b00001111);
+        p.repair();
+        assert_eq!(*p.bytes.last().unwrap(), 0b00001111);
+    }
+
+    /// Verify that the repair that occurs within the not operation works.
+    ///
+    /// If the repair is incorrect, we would expect to see `unoccupied_count > natoms_total` after
+    /// the not.
+    #[test]
+    fn check_not_repair() {
+        let mut solvent = Structure::open_gro("structures/water.gro").unwrap();
+        // Make sure we have a structure size that does neatly fit in some number of bytes.
+        if solvent.natoms() % 8 == 0 {
+            solvent.atoms.pop();
+        }
+        let natoms = solvent.natoms();
+        let size = UVec3::new(3, 5, 7);
+        let mut placemap = PlaceMap::new(&solvent, size);
+        let natoms_total = natoms * placemap.n_cells();
+        let n_occupied = 0;
+
+        // Set some atom to be occupied.
+        placemap
+            .get_mut(UVec3::new(1, 3, 5))
+            .unwrap()
+            .set_occupied(161);
+        // Update natoms_total and n_occupied to reflect this.
+        let natoms_total = natoms_total - 1;
+        let n_occupied = n_occupied + 1;
+
+        assert_eq!(placemap.unoccupied_count(), natoms_total);
+        assert_eq!(placemap.occupied_count(), n_occupied);
+
+        placemap = !placemap;
+
+        assert_eq!(placemap.unoccupied_count(), n_occupied);
+        assert_eq!(placemap.occupied_count(), natoms_total);
+
+        placemap = !placemap;
+
+        assert_eq!(placemap.unoccupied_count(), natoms_total);
+        assert_eq!(placemap.occupied_count(), n_occupied);
     }
 }
