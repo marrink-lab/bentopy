46f692306044ad70399219965b2da99a58f7b738
Marieke Westendorp <ma3ke.cyber@gmail.com>
2025-03-07 13:53:39 +0100

    pack: Introduce structure::ParsePdbError and improve parsing errors

--- src/pack/structure.rs
+++ src/pack/structure.rs
@@ -1,4 +1,5 @@
 use std::io::{BufRead, BufReader};
+use std::num::NonZeroUsize;
 
 use eightyseven::reader::{ParseList, ReadGro};
 use eightyseven::structure::{AtomName, AtomNum, ResName, ResNum};
@@ -49,19 +50,49 @@ impl ReadGro<Atom> for Structure {
     }
 }
 
+#[non_exhaustive]
+#[derive(Debug)]
+pub enum ParsePdbError {
+    IOError(std::io::Error),
+    BadPositionValue {
+        err: std::num::ParseFloatError,
+        /// Line number.
+        ln: NonZeroUsize,
+    },
+}
+
+impl std::error::Error for ParsePdbError {}
+
+impl std::fmt::Display for ParsePdbError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::IOError(err) => write!(f, "IO error: {err}"),
+            Self::BadPositionValue { err, ln } => {
+                write!(f, "Could not parse record on line {ln}: {err}")
+            }
+        }
+    }
+}
+
+impl From<std::io::Error> for ParsePdbError {
+    fn from(err: std::io::Error) -> Self {
+        Self::IOError(err)
+    }
+}
+
 impl Structure {
-    pub fn read_from_pdb_file(
-        file: std::fs::File,
-    ) -> Result<Structure, eightyseven::reader::ParseGroError> {
+    pub fn read_from_pdb_file(file: std::fs::File) -> Result<Structure, ParsePdbError> {
         let reader = BufReader::new(file);
 
         let mut atoms = Vec::new();
-        for line in reader.lines() {
+        for (ln, line) in reader.lines().enumerate() {
             let line = line?;
             if line.starts_with("ATOM") || line.starts_with("HETATM") {
-                let x = line[30..38].trim().parse().unwrap();
-                let y = line[38..46].trim().parse().unwrap();
-                let z = line[46..54].trim().parse().unwrap();
+                let ln = NonZeroUsize::new(ln + 1).unwrap(); // We know ln + 1 >= 1 because ln >= 0.
+                let bad_position = |err| ParsePdbError::BadPositionValue { err, ln };
+                let x = line[30..38].trim().parse().map_err(bad_position)?;
+                let y = line[38..46].trim().parse().map_err(bad_position)?;
+                let z = line[46..54].trim().parse().map_err(bad_position)?;
                 let atom = Atom::new(x, y, z) / 10.0; // Convert from Å to nm.
                 atoms.push(atom);
             }
