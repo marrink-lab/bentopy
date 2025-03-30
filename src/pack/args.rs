12681b4c641afffb4dddcf882c9784bfba94d71c
Marieke Westendorp <ma3ke.cyber@gmail.com>
2025-03-30 15:19:54 +0200

    Add git commit hash information to bentopy-pack --version output

--- src/pack/args.rs
+++ src/pack/args.rs
@@ -2,9 +2,36 @@ use std::path::PathBuf;
 
 use clap::{Parser, ValueEnum};
 
+/// Provide additional version information, including the current git hash.
+mod version {
+    pub(crate) struct Version {
+        pkg_version: &'static str,
+        git_version: &'static str,
+    }
+
+    impl Into<clap::builder::Str> for Version {
+        fn into(self) -> clap::builder::Str {
+            let Self {
+                pkg_version,
+                git_version,
+            } = self;
+            format!("{pkg_version} ({git_version})").into()
+        }
+    }
+
+    pub(crate) const VERSION: Version = Version {
+        pkg_version: env!("CARGO_PKG_VERSION"),
+        git_version: git_version::git_version!(
+            args = ["--broken", "--always", "--exclude", "*"],
+            prefix = "git:",
+            fallback = "?"
+        ),
+    };
+}
+
 /// Pack a space.
 #[derive(Debug, Parser)]
-#[command(version)]
+#[command(version = version::VERSION)]
 pub struct Args {
     // Configuration input file to define the run (json).
     pub config: PathBuf,
