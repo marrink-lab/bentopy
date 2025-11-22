cda325512ce713477a846637a35cc9585d81a145
Marieke Westendorp <ma3ke.cyber@gmail.com>
2025-11-22 13:37:45 +0100

    Bump version to 0.1.0-alpha.1 and bring minimum python version to 3.12
    Preparing for upload to PyPI.

--- release.sh
+++ release.sh
@@ -3,5 +3,9 @@ set -xue
 
 docker pull quay.io/pypa/manylinux2014_x86_64
 docker run --rm -v `pwd`:/io quay.io/pypa/manylinux2014_x86_64 bash /io/build-wheels.sh
+
 # Not yet an actual release script, but building the wheels is a step along 
 # the way. Will be expanded.
+#
+# twine upload dist/*-manylinux2014_x86_64.manylinux_2_17_x86_64.whl --repository=testpypi --verbose
+# twine upload dist/*-manylinux2014_x86_64.manylinux_2_17_x86_64.whl --repository=pypi     --verbose
