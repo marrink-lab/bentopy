7ae3a62e9f7c5f8e19c726bade0bae2723c939ec
Marieke Westendorp <ma3ke.cyber@gmail.com>
2025-12-04 21:23:23 +0100

    core: Move version module from pack to core
    This makes it generally accessible and now we can easily use it in all
    bentopy subcommands.

--- src/pack/args.rs
+++ src/pack/args.rs
@@ -4,7 +4,7 @@ use clap::{Parser, ValueEnum};
 
 /// Pack a space.
 #[derive(Debug, Parser)]
-#[command(version = version::VERSION)]
+#[command(version = bentopy::core::version::VERSION)]
 pub struct Args {
     // Configuration input file to define the run (json).
     pub config: PathBuf,
@@ -74,31 +74,4 @@ pub enum RearrangeMethod {
     BoundingSphere,
     /// Keep the arrangement specified in the input file.
     None,
-}
-
-/// Provide additional version information, including the current git hash.
-mod version {
-    pub(crate) struct Version {
-        pkg_version: &'static str,
-        git_version: &'static str,
-    }
-
-    impl Into<clap::builder::Str> for Version {
-        fn into(self) -> clap::builder::Str {
-            let Self {
-                pkg_version,
-                git_version,
-            } = self;
-            format!("{pkg_version} ({git_version})").into()
-        }
-    }
-
-    pub(crate) const VERSION: Version = Version {
-        pkg_version: env!("CARGO_PKG_VERSION"),
-        git_version: git_version::git_version!(
-            args = ["--broken", "--always", "--exclude", "*"],
-            prefix = "git:",
-            fallback = "release"
-        ),
-    };
 }
