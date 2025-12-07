7dc05cb32e2bac5016288f4fefa07bfdeca83f0d
Marieke Westendorp <ma3ke.cyber@gmail.com>
2025-12-07 22:26:49 +0100

    core: Move version module to a file

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,36 +1,3 @@
 pub mod config;
 pub mod placement;
-
-/// Provide additional version information, including the current git hash.
-pub mod version {
-    pub struct Version {
-        pkg_version: &'static str,
-        git_version: &'static str,
-    }
-
-    impl std::fmt::Display for Version {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            let Self {
-                pkg_version,
-                git_version,
-            } = self;
-            write!(f, "{pkg_version} ({git_version})")
-        }
-    }
-
-    // This makes it possible to use it as the version that Clap displays for a command.
-    impl Into<clap::builder::Str> for Version {
-        fn into(self) -> clap::builder::Str {
-            self.to_string().into()
-        }
-    }
-
-    pub const VERSION: Version = Version {
-        pkg_version: env!("CARGO_PKG_VERSION"),
-        git_version: git_version::git_version!(
-            args = ["--broken", "--always", "--exclude", "*"],
-            prefix = "git:",
-            fallback = "release"
-        ),
-    };
-}
+pub mod version;
